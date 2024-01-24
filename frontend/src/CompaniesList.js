722e8e33aafa4f7b843dbb714187c223d9f718ee
Max Shpungin <maxsdance@gmail.com>
2024-01-23 17:51:32 -0800

    with changes

--- frontend/src/CompaniesList.js
+++ frontend/src/CompaniesList.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import JoblyApi from "./api";
+
 /**
  * CompaniesList - get all companies from API and show company data
  *
@@ -12,6 +15,26 @@
 
 function CompaniesList(){
   console.log("CompaniesList");
+
+  const [ companiesData, setCompaniesData ]
+    = useState({data:null, isLoading:false});
+
+
+  //this page renders and we want to grab all the companies as soon as we can
+  // use effect to make api request to get the companies
+
+  useEffect(function fetchAndSetCompaniesData(){
+    async function getCompanies(companiesData){
+      let companyResults = await JoblyApi.getAllCompanies();
+
+      setCompaniesData({
+        data: companyResults,
+        isLoading: true
+      }, );//TODO: come back to me
+    }
+  }, [companiesData]);
+
+  if (companiesData.isLoading) return <i>...R2D2 noises ...</i>;
 
   return (
     <div className="CompaniesList">
