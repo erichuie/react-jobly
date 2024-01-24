f4e53fe223a1b345985deae05757fae3c9ba8237
Eric Huie <erichuie97@gmail.com>
2024-01-23 18:01:03 -0800

    added merged code

--- frontend/src/CompaniesList.js
+++ frontend/src/CompaniesList.js
@@ -11,14 +11,33 @@
  * RoutesList -> CompaniesList -> SearchForm, CompanyCard
 */
 
-function CompaniesList(){
+function CompaniesList() {
   console.log("CompaniesList");
+  const [companiesData, setCompaniesData]
+    = useState({ data: null, isLoading: false });
+
+
+  //this page renders and we want to grab all the companies as soon as we can
+  // use effect to make api request to get the companies
+
+  useEffect(function fetchAndSetCompaniesData() {
+    async function getCompanies(companiesData) {
+      let companyResults = await JoblyApi.getAllCompanies();
+
+      setCompaniesData({
+        data: companyResults,
+        isLoading: true
+      },);//TODO: come back to me
+    }
+  }, [companiesData]);
+
+  if (companiesData.isLoading) return <i>...R2D2 noises ...</i>;
 
   return (
     <div className="CompaniesList">
 
     </div>
-  )
+  );
 }
 
 export default CompaniesList;
